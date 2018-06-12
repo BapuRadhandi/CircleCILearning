4dc5c70fedbccdd292c0dce8d9376f5d1f1d2104
bapur <bapur@192.168.1.3>
2018-06-12 20:54:36 +0530

    First Commit

---
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -0,0 +1,23 @@
+package CircleCIIntegration.CircleCIIntegration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CircleCiTest {
+	
+	public static void main(String[]args) {
+		
+		
+		System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "/Drivers/mac/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://google.com");
+		System.out.println("Hello World");
+
+		
+		
+		
+	}
+
+}
