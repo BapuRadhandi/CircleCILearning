b89e3cbb97817819a0431f39de2b7e1014f553f8
bapur <bapur@192.168.1.3>
2018-06-12 21:21:23 +0530

    added linux driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -10,7 +10,7 @@ public class CircleCiTest {
 		
 		System.out.println("launching chrome browser");
 		System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/Drivers/mac/chromedriver");
+                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://google.com");
 		System.out.println("Hello World");
