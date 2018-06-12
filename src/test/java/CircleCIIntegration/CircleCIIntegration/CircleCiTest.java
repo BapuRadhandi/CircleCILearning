0600f231b8d598455e955949f8caa1cf794e0589
bapur <bapur@192.168.1.3>
2018-06-12 23:52:25 +0530

    test

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -1,6 +1,6 @@
 package CircleCIIntegration.CircleCIIntegration;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CircleCiTest {
