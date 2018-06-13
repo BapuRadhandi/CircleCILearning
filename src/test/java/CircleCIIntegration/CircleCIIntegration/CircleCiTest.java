9360f315aa8c54c4c1056f1483b57cc82829642b
bapur <bapur@192.168.1.3>
2018-06-13 16:41:50 +0530

    driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -12,10 +12,8 @@ public class CircleCiTest {
 		
 		
 		System.out.println("launching chrome browser");
-		System.out.println(System.getProperty("user.dir"));
-		System.out.println(System.getProperty("usr.dir"));
 		System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/CircleCIIntegration/Drivers/linux/chromedriver");
+                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://google.com");
 		System.out.println("Hello World");
