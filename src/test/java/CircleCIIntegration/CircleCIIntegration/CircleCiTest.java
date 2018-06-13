c49cc6f5b9bad502e9a73050d07e7612b5877fe5
bapur <bapur@192.168.1.3>
2018-06-13 16:53:05 +0530

    driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -2,6 +2,7 @@ package CircleCIIntegration.CircleCIIntegration;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 public class CircleCiTest {
@@ -11,14 +12,18 @@ public class CircleCiTest {
 	public void googleTest() {
 		
 		
-		System.out.println("launching chrome browser");
-		System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
-		WebDriver driver = new ChromeDriver();
+//		System.out.println("launching chrome browser");
+//		System.setProperty("webdriver.chrome.driver",
+//                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+
+//		System.out.println("launching chrome browser");
+//		System.setProperty("webdriver.chrome.driver",
+//                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
+		WebDriver driver = new HtmlUnitDriver();
 		driver.navigate().to("http://google.com");
 		System.out.println("Hello World");
-		System.out.println("test");
-		System.out.println("sample test");
+		System.out.println(driver.getTitle());
 		
 		
 		
