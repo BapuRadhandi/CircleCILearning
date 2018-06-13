abe936d7b6ff0b8afed3f033f7c97a923409b997
bapur <bapur@192.168.1.3>
2018-06-13 16:31:44 +0530

    yml

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -11,11 +11,11 @@ public class CircleCiTest {
 	public void googleTest() {
 		
 		
-//		System.out.println("launching chrome browser");
-//		System.setProperty("webdriver.chrome.driver",
-//                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
-//		WebDriver driver = new ChromeDriver();
-//		driver.navigate().to("http://google.com");
+		System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://google.com");
 		System.out.println("Hello World");
 		System.out.println("test");
 		System.out.println("sample test");
