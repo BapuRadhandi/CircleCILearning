95391c277f45a791471a5bf5a351f5c9c9ff65a8
bapur <bapur@192.168.1.3>
2018-06-13 16:13:14 +0530

    yml

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -2,17 +2,20 @@ package CircleCIIntegration.CircleCIIntegration;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class CircleCiTest {
 	
-	public static void main(String[]args) {
+	
+	@Test
+	public void googleTest() {
 		
 		
-		System.out.println("launching chrome browser");
-		System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://google.com");
+//		System.out.println("launching chrome browser");
+//		System.setProperty("webdriver.chrome.driver",
+//                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+//		driver.navigate().to("http://google.com");
 		System.out.println("Hello World");
 		System.out.println("test");
 
