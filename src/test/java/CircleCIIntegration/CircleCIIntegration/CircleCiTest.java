80d80a16db8357b32ad82f930518608cc268ffa9
bapur <bapur@172.16.50.49>
2018-06-18 11:10:47 +0530

    driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -23,7 +23,7 @@ public class CircleCiTest {
 //		System.out.println("launching chrome browser");
 //		System.setProperty("webdriver.chrome.driver",
 //                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
-		WebDriver driver = new HtmlUnitDriver();
+		HtmlUnitDriver driver = new HtmlUnitDriver();
 		
 		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 //		driver.navigate().to("http://google.com");
