f21f25e53beb7b914f29b0ba51774de630ac97d8
bapur <bapur@172.16.50.49>
2018-06-18 11:08:22 +0530

    timeout

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -1,8 +1,11 @@
 package CircleCIIntegration.CircleCIIntegration;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class CircleCiTest {
@@ -21,12 +24,19 @@ public class CircleCiTest {
 //		System.setProperty("webdriver.chrome.driver",
 //                System.getProperty("user.dir") + "/Drivers/linux/chromedriver");
 		WebDriver driver = new HtmlUnitDriver();
+		
+		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 //		driver.navigate().to("http://google.com");
 		driver.navigate().to("http://pkr4jm9q0slz.live.wavemakeronline.com/LIS/#/Login");
 //		http://a4a031c3f6a2311e8a8060a9f3430e67-834246212.us-east-2.elb.amazonaws.com/LIS/#/Login
+		
+		
 		System.out.println("Hello World");
 		System.out.println(driver.getTitle());
 		Thread.sleep(5000);
+		
+		
+			 //  driver.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//td[2]/input")));
 		driver.findElement(By.xpath("//input[@name='usernametext']")).sendKeys("admin");
 		driver.findElement(By.xpath(".//*[@id='wm-app-content']/div/div/div/div/div/div/div/div/div/form/div/div[2]/div/div/input")).sendKeys("admin");
 		driver.findElement(By.xpath(".//*[@id='wm-app-content']/div/div/div/div/div/div/div/div/div/form/div/div[3]/div/div/button")).click();
