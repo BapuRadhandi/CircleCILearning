fc61dd89aca6e4637671bd6115944b792c759d0a
bapur <bapur@172.16.50.49>
2018-07-03 14:21:45 +0530

    driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -2,6 +2,7 @@ package CircleCIIntegration.CircleCIIntegration;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.collections.bag.SynchronizedSortedBag;
 import org.openqa.selenium.*;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
@@ -24,11 +25,13 @@ public class CircleCiTest {
 		((DesiredCapabilities) caps).setCapability(
 		PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
 		"/Users/bapur/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs"
+//		System.getProperty("user.dir") +"/Drivers/mac/phantomjs"
 		);
 		WebDriver driver = new PhantomJSDriver(caps);
 		driver.get("https://www.google.co.in");
 		System.out.println(driver.getTitle());
 		System.out.println("driver instance :" + driver);
+//		System.getProperty("user.dir" +"/Users/bapur/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
  				
 	}
 
