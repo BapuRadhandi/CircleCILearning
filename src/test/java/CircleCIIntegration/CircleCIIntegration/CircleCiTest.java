f69d662687a75e009ac9c92adfbda9eb1b9d41af
bapur <bapur@172.16.50.49>
2018-07-07 11:18:13 +0530

    updated driver

--- src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
+++ src/test/java/CircleCIIntegration/CircleCIIntegration/CircleCiTest.java
@@ -1,12 +1,11 @@
 package CircleCIIntegration.CircleCIIntegration;
 
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.collections.bag.SynchronizedSortedBag;
 import org.openqa.selenium.*;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -19,19 +18,30 @@ public class CircleCiTest {
 	public void googleTest() throws InterruptedException {
 		
  		
-		Capabilities caps = new DesiredCapabilities();
-		((DesiredCapabilities) caps).setJavascriptEnabled(true);
-//		((DesiredCapabilities) caps).setCapability(“takesScreenshot”, true);
-		((DesiredCapabilities) caps).setCapability(
-		PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-		"/Users/bapur/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs"
-//		System.getProperty("user.dir") +"/Drivers/mac/phantomjs"
-		);
-		WebDriver driver = new PhantomJSDriver(caps);
-		driver.get("https://www.google.co.in");
-		System.out.println(driver.getTitle());
-		System.out.println("driver instance :" + driver);
-//		System.getProperty("user.dir" +"/Users/bapur/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+	
+
+			Capabilities caps = new DesiredCapabilities();
+			((DesiredCapabilities) caps).setJavascriptEnabled(true);
+			((DesiredCapabilities) caps).setAcceptInsecureCerts(true);
+//			((DesiredCapabilities) caps).setCapability(“takesScreenshot”, true);
+			
+			((DesiredCapabilities) caps).setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			((DesiredCapabilities) caps).setCapability(
+			PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+//			"/Users/bapur/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs"
+			"Drivers/mac/phantomjs"
+			);
+							
+			WebDriver driver = new PhantomJSDriver(caps);	
+		
+		System.out.println("driver instance is :"+ driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		driver.get("http://pk54ysqfjstw.cloud.wavemakeronline.com/s4_lis_accessioning/#/Login?redirectTo=Accession");
+		driver.get("https://lplus-qa.sema4genomics.com/s4-lis-accessioning/#/Login");
+		Thread.sleep(5000);
+		System.out.println("Title is : "+driver.getTitle());
+
+
  				
 	}
 
